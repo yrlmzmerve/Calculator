a1a8bc12f01d630a684607fb92340e654cb1070c
yrlmzmerve <vier-und.achtzig-tm@hotmail.com>
2018-05-06 23:55:23 +0300

    calculater wth c# console app

---
+++ WindowsFormsApplication7/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication7
+{
+    public partial class Form1 : Form
+    {
+        double num1, num2;
+        string sign = "";
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+                textBox1.Text = textBox1.Text + "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox1.Text + "2";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "0";
+        }
+
+        private void button11_Click(object sender, EventArgs e)//for calculate
+        {
+
+            if (sign == "+")
+                textBox1.Text = Convert.ToString(num1 + num2);
+            if (sign == "-")
+                textBox1.Text = Convert.ToString(num1 - num2);
+            if (sign == "*")
+                textBox1.Text = Convert.ToString(num1 * num2);
+            if (sign == "%")
+                textBox1.Text = Convert.ToString(num1 % num2);
+        }   
+
+        private void button12_Click(object sender, EventArgs e) //+
+        {
+            num1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text += "+";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)//text
+        {
+            
+        }
+
+        private void button13_Click(object sender, EventArgs e) // -
+        {
+            num1 = Convert.ToDouble(textBox1.Text);
+            sign = "-";
+            textBox1.Text = "-";
+        }
+        private void button14_Click(object sender, EventArgs e) // *
+        {
+            num1 = Convert.ToDouble(textBox1.Text);
+            sign = "*";
+            textBox1.Text = "*";
+        }
+        private void button15_Click(object sender, EventArgs e) // %
+        {
+            num1 = Convert.ToDouble(textBox1.Text);
+            sign = "%";
+            textBox1.Text = "%";
+        }
+        private void button16_Click(object sender, EventArgs e) // C 
+        {
+            num1 = 0;
+            num2 = 0;
+            sign = "";
+        }
+
+
+    
+    }
+}
